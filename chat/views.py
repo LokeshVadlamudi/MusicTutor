ee042d956802d273bdd86e5be3d53ce666e7ed98
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2021-05-08 17:26:52 -0700

    can't chat if not logged in

--- chat/views.py
+++ chat/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 # Create your views here.
-
+@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html', {})
 
-
+@login_required(login_url='login')
 def room(request, room_name):
     return render(request, 'chatroom.html', {'room_name': room_name})
